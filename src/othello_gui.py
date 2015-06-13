050a09f373241fff2c044c04412ccf3384bf87b7
yassu <mathyassu@gmail.com>
2015-06-14 06:44:37 +0900

    class of each button

--- src/othello_gui.py
+++ src/othello_gui.py
@@ -9,6 +9,38 @@ class OthelloCellButton(Button):
             kw['background'] = 'lime green'
         Button.__init__(self, master, kw)
 
+class StartButton(Button):
+    def __init__(self, master=None, **kw):
+        if 'text' not in kw.keys():
+            kw['text'] = 'Start'
+        Button.__init__(self, master, kw)
+
+    def get_name(self):
+        return 'Start'
+
+class ClearButton(Button):
+    def __init__(self, master=None, **kw):
+        if 'text' not in kw.keys():
+            kw['text'] = 'Clear'
+        Button.__init__(self, master, kw)
+
+class LoadDataButton(Button):
+    def __init__(self, master=None, **kw):
+        if 'text' not in kw.keys():
+            kw['text'] = 'Load Data'
+        Button.__init__(self, master, kw)
+
+class AutoPutButton(Button):
+    def __init__(self, master=None, **kw):
+        if 'text' not in kw.keys():
+            kw['text'] = 'Auto put'
+        Button.__init__(self, master, kw)
+
+class AllPutButton(Button):
+    def __init__(self, master=None, **kw):
+        if 'text' not in kw.keys():
+            kw['text'] = 'All put'
+        Button.__init__(self, master, kw)
 
 class OthelloBord(Frame):
 
@@ -41,19 +73,19 @@ class SideButtonBarFrame(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
 
-        self._start_button = Button(self, text='start')
+        self._start_button = StartButton(self)
         self._start_button.grid(row=0, column=0)
 
-        self._clear_button = Button(self, text='clear')
+        self._clear_button = ClearButton(self)
         self._clear_button.grid(row=1, column=0)
 
-        self._load_data_button = Button(self, text='Load Data')
+        self._load_data_button = LoadDataButton(self)
         self._load_data_button.grid(row=2, column=0)
 
-        self._auto_put_button = Button(self, text='Auto put')
+        self._auto_put_button = AutoPutButton(self)
         self._auto_put_button.grid(row=3, column=0)
 
-        self._all_put_button = Button(self, text='All put')
+        self._all_put_button = AllPutButton(self)
         self._all_put_button.grid(row=4, column=0)
 
 
