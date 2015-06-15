3945e355249789b0c1b138db9d5454f5e41d70d8
yassu <mathyassu@gmail.com>
2015-06-16 07:11:17 +0900

    get_parser and simple main

---
+++ src/main.py
@@ -0,0 +1,27 @@
+from optparse import OptionParser
+from sys import exit
+
+def get_parser():
+    parser = OptionParser('othello [option]')
+    parser.add_option('--gui', action='store_true', default=False, dest='gui')
+    parser.add_option('--cui', action='store_true', default=False, dest='cui')
+    return parser
+
+def main():
+    parser = get_parser()
+    (options, _) = parser.parse_args()
+
+    print('options.cui: {}'.format(options.cui))
+    print('options.gui: {}'.format(options.gui))
+
+    if options.cui and options.gui:
+        print("Can't set both cui and gui option.")
+        exit()
+    elif not options.cui:
+        runner = GuiRunner()
+    else:
+        runner = CuiRunner()
+    runner.main()
+
+if __name__ == '__main__':
+    main()
